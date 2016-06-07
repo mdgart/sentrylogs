5c81ef6107b07c6d37d98b863e91bf1d40d87ab6
Peter Bittner <django@bittner.it>
2016-06-07 20:56:02 +0200

    Add tox configuration and static code analysis (flake8, pylint)
    Convert README to reStructuredText (was: MarkDown) for PyPI
    
    Add full text of license (identical to Sentry project)
    
    Use standard naming for contributors file
    
    Move package information to __init__ file

--- sentrylogs/__init__.py
+++ sentrylogs/__init__.py
@@ -1,1 +1,8 @@
-
+"""
+Send logs to Django Sentry.
+"""
+__author__ = 'Mauro De Giorgi'
+__author_email__ = 'mauro.degiorgi@gmail.com'
+__license__ = 'BSD'
+__url__ = 'https://github.com/mdgart/sentrylogs'
+__version__ = '1.1.0.dev'
