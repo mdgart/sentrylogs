81efe87aab2338f09ee7b126ddb356a4266d4a9e
Peter Bittner <django@bittner.it>
2016-06-09 04:39:44 +0200

    Add abstract base class for logging to Sentry
    Remove obsolete /nginx.py file

--- sentrylogs/parsers/__init__.py
+++ sentrylogs/parsers/__init__.py
@@ -1,3 +1,49 @@
 """
 Log file parsers provided by Sentry Logs
 """
+import tailer  # same functionality as UNIX tail in python
+
+from ..helpers import send_message
+
+
+class Parser(object):
+    """Abstract base class for any parser"""
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.logger = self.__doc__.strip()
+        self.message = None
+        self.extended_message = None
+        self.params = None
+        self.site = None
+
+    def follow_tail(self):
+        """
+        Read (tail and follow) the log file, parse entries and send messages
+        to Sentry using Raven.
+        """
+        for line in tailer.follow(open(self.filepath)):
+            self.message = None
+            self.extended_message = None
+            self.params = None
+            self.site = None
+
+            self.parse(line)
+            send_message(self.message,
+                         self.extended_message,
+                         self.params,
+                         self.site,
+                         self.logger)
+
+    def parse(self, line):
+        """
+        Parse a line of a log file.  Must be overridden by the subclass.
+        The implementation must set these properties:
+
+        - ``message`` (string)
+        - ``extended_message`` (string)
+        - ``params`` (list of string)
+        - ``site`` (string)
+        """
+        raise NotImplementedError('parse() method must set: '
+                                  'message, extended_message, params, site')
--- sentrylogs/parsers/nginx.py
+++ sentrylogs/parsers/nginx.py
@@ -11,27 +11,54 @@ upstream: "http://0.0.0.0:9000/api/megapage/poll/?cursor=1354216956",
 host: "165.225.132.103",
 referrer: "http://165.225.132.103/megapage/"
 """
+from . import Parser
+from ..conf.settings import NGINX_ERROR_PATH
 
 
-def nginx_error_parser(line):
-    """
-    Parse a line of the Nginx error log.
-    """
-    csv_list = line.split(",")
-    date_time_message = csv_list.pop(0).split(" ", 2)
-    otherinfo = dict()
+class Nginx(Parser):
+    """Nginx error logs"""
 
-    for item in csv_list:
-        key_value_pair = item.split(":", 1)
-        key = key_value_pair[0].strip()
+    def __init__(self):
+        super(Nginx, self).__init__(NGINX_ERROR_PATH)
 
-        if len(key_value_pair) > 1:
-            value = key_value_pair[1].strip()
-            if not value:
+    def parse(self, line):
+        """Parse a line of the Nginx error log"""
+        csv_list = line.split(",")
+        date_time_message = csv_list.pop(0).split(" ", 2)
+        otherinfo = dict()
+
+        for item in csv_list:
+            key_value_pair = item.split(":", 1)
+            key = key_value_pair[0].strip()
+
+            if len(key_value_pair) > 1:
+                value = key_value_pair[1].strip()
+                if not value:
+                    value = "-"
+            else:
                 value = "-"
-        else:
-            value = "-"
 
-        otherinfo[key] = value
+            otherinfo[key] = value
 
-    return date_time_message, otherinfo
+        self.message = '%s' % date_time_message[2]
+        self.extended_message = '%s\n' \
+                                'Date: %s\n' \
+                                'Time: %s\n' \
+                                'Request: %s\n' \
+                                'Referrer: %s\n' \
+                                'Server: %s\n' \
+                                'Client: %s\n' \
+                                'Host: %s\n' \
+                                'Upstream: %s\n'
+        self.params = [
+            date_time_message[2],
+            date_time_message[0],
+            date_time_message[1],
+            otherinfo.get("request", "-"),
+            otherinfo.get("referrer", "-"),
+            otherinfo.get("server", "-"),
+            otherinfo.get("client", "-"),
+            otherinfo.get("host", "-"),
+            otherinfo.get("upstream", "-"),
+        ]
+        self.site = otherinfo.get("referrer", "-")
